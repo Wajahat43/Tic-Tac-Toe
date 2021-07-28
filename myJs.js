50b8628395be0b635baf95889147512092457d4b
Wajahat <aliwjahat44@yahoo.com>
2021-07-28 11:54:28 +0500

    Added a function which takes two arguments. This function simulates the behavior of a single Rock Paper Scissors Round and then returns a string mentioning whether the player win or loose.

--- myJs.js
+++ myJs.js
@@ -9,3 +9,29 @@ function computerPlay(){
     }
 }
 
+function playRound(playerSelection, computerSelection){
+    let inPlayerSelection = playerSelection.toLowerCase();
+    let inComputerSelection = computerSelection.toLowerCase();
+
+    //Player wins by default.
+    let playerWinStatus = 1;
+
+    if(inPlayerSelection === inComputerSelection){
+        return "Its a DRAW! Computer and Player both had same choices.";
+    }
+    //loose Scenarios
+    if(inPlayerSelection==="rock" && inComputerSelection == "paper"){
+        playerWinStatus = 0;
+    } else if(inPlayerSelection=="paper" && inComputerSelection =="scissors"){
+        playerWinStatus =0;
+    } else if(inPlayerSelection==="scissors" && inComputerSelection == "rock"){
+        playerWinStatus =0;
+    }
+
+    if(playerWinStatus ===1){
+        return "You win! " + playerSelection.toUpperCase() + " beats " + computerSelection.toUpperCase();
+    } else if(playerWinStatus===0){
+        return "You Loose! " + computerSelection.toUpperCase()+ " beats " +playerSelection.toUpperCase();
+    }
+
+}
